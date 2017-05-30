2a249aa07d9791f9c1598406ea621a2ca3a000c0
Hugo MOCHET <mochet.hugo@gmail.com>
2017-05-30 11:56:06 +0200

    WIP Translation of tci-online

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ from core.views import *
 # WSGI
 application = app
 
+
+def get_translations_files(language):
+    return "readme.md"
+
+
 if __name__ == '__main__':
     app.run(debug=app.config.get('DEBUG'), host=app.config.get('HOST'),
             port=app.config.get('PORT'))
--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
 from flask import render_template, request, abort
 from tcidatabase.models import SurveyToken, Score
 from tcidata import get_tci
+from tcii18n.translator import Translator
+from tcii18n.template import flask_methods
 
 from . import app
+from app import get_translations_files
 
 
 @app.route('/')
@@ -11,8 +14,18 @@ def home():
 
 
 @app.route('/<string:token>')
-def survey(token):
+def language(token):
     try:
+        token = SurveyToken.objects.get(key=token, usage_date=None)
+    except SurveyToken.DoesNotExist:
+        return render_template('error.html'), 401
+    return render_template('language.html', token=token)
+
+
+@app.route('/<string:language>/<string:token>')
+def survey(language, token):
+    try:
+        translator = Translator(get_translations_files(language))
         token = SurveyToken.objects.get(key=token, usage_date=None)
     except SurveyToken.DoesNotExist:
         return render_template('error.html'), 401
@@ -22,7 +35,8 @@ def survey(token):
         abort(501, 'The token has a inexisting "{}" survey associated.'.format(
               token.survey))
     questions = tci.get('questions')
-    return render_template('survey.html', questions=questions, token=token)
+    return render_template('survey.html', questions=questions, token=token,
+                           trans=translator.translate)
 
 
 @app.route('/end', methods=['post'])
